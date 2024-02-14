a54fe4cdfb0dafb3b1b0cc78cd4eb600872ad7ca
João Gabriel Luciano <jgluciano.luz@gmail.com>
2024-02-14 12:16:39 -0300

    Addition of the AOS library and refactoring of styles for better readability

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
+const dataDoEvento = new Date("Mar 12, 2024 19:00:00");
+const timeStampEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function () {
+  const agora = new Date();
+  const timeStampAtual = agora.getTime();
+
+  const distanciaAteEvento = timeStampEvento - timeStampAtual;
+  const diasMs = 1000 * 60 * 60 * 24;
+  const horasMs = 1000 * 60 * 60;
+  const minutosMs = 1000 * 60;
+
+  const diasAteEvento = Math.floor(distanciaAteEvento / (diasMs));
+  const horasAteEvento = Math.floor((distanciaAteEvento % diasMs) / (horasMs));
+  const minutosAteEvento = Math.floor((distanciaAteEvento % horasMs) / (minutosMs));
+  const segundosAteEvento = Math.floor((distanciaAteEvento % minutosMs) / 1000);
+
+  document.getElementById('counter').innerHTML = `${diasAteEvento}d ${horasAteEvento}h ${minutosAteEvento}m ${segundosAteEvento}s`
+  document.getElementById('counter-text').innerHTML = 'A maratona começa em:&nbsp;';
+
+  if (distanciaAteEvento <= 0) {
+    clearInterval(contaAsHoras);
+    document.getElementById('counter-text').innerHTML = 'A maratona&nbsp;';
+    document.getElementById('counter').innerHTML = 'ja iniciou!'
+  }
+
+}, 1000);
+
+setTimeout(function() {
+  AOS.init();
+}, 1000);
+
 function smoothScroll(target, duration) {
   var targetElement = document.querySelector(target);
   var targetPosition = targetElement.offsetTop;
@@ -32,31 +64,3 @@ links.forEach(link => {
       smoothScroll(target, duration);
   });
 });
-
-const dataDoEvento = new Date("Mar 12, 2024 19:00:00");
-const timeStampEvento = dataDoEvento.getTime();
-
-const contaAsHoras = setInterval(function () {
-  const agora = new Date();
-  const timeStampAtual = agora.getTime();
-
-  const distanciaAteEvento = timeStampEvento - timeStampAtual;
-  const diasMs = 1000 * 60 * 60 * 24;
-  const horasMs = 1000 * 60 * 60;
-  const minutosMs = 1000 * 60;
-
-  const diasAteEvento = Math.floor(distanciaAteEvento / (diasMs));
-  const horasAteEvento = Math.floor((distanciaAteEvento % diasMs) / (horasMs));
-  const minutosAteEvento = Math.floor((distanciaAteEvento % horasMs) / (minutosMs));
-  const segundosAteEvento = Math.floor((distanciaAteEvento % minutosMs) / 1000);
-
-  document.getElementById('counter').innerHTML = `${diasAteEvento}d ${horasAteEvento}h ${minutosAteEvento}m ${segundosAteEvento}s`
-  document.getElementById('counter-text').innerHTML = 'A maratona começa em:&nbsp;';
-
-  if (distanciaAteEvento <= 0) {
-    clearInterval(contaAsHoras);
-    document.getElementById('counter-text').innerHTML = 'A maratona&nbsp;';
-    document.getElementById('counter').innerHTML = 'ja iniciou!'
-  }
-
-}, 1000);
